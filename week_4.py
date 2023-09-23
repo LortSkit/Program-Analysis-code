4b977f4d523e84ef73de3dd6943346526f81faae
Chetraru Ion <s204710@student.dtu.dk>
2023-09-23 13:59:34 +0200

    Testing implemented We made testing for all the methods in Simple.java

--- week_4.py
+++ week_4.py
@@ -1,6 +1,7 @@
 import json
 
-data = json.loads(open('./course-02242-examples/decompiled/dtu/compute/exec/Calls.json').read())
+data = json.loads(
+    open('./course-02242-examples/decompiled/dtu/compute/exec/Simple.json').read())
 
 
 def cleanByteCode(bytecofr):
@@ -17,7 +18,6 @@ def cleanByteCode(bytecofr):
 
 def getCleanMethods(data):
     methods = []
-    # print(data['methods'])
     for method in data["methods"]:
         for annotation in method["annotations"]:
             if annotation["type"] == "dtu/compute/exec/Case":
@@ -25,7 +25,6 @@ def getCleanMethods(data):
                     {"name": method["name"], "bytecode": cleanByteCode(method["code"]["bytecode"]),
                      "max_stack": method["code"]["max_stack"], "max_locals": method["code"]["max_locals"]})
                 break
-            # return []
 
     return methods
 
@@ -86,82 +85,86 @@ class Interpreter:
         self.log(f"Loading {v} at index {b['index']}")
         self.stack.append((l, (s+[v])[-self.program["max_stack"]:], pc+1))
         return True, b
-    
-    
-    def store(self,b): 
+
+    def store(self, b):
         (l, s, pc) = self.stack.pop(-1)
         l[b["index"]] = s[-1]
         self.log(f"Storing {s[-1]} at index {b['index']}")
         self.stack.append((l, s, pc+1))
         return True, b
-    
-    def if1(self,b):
+
+    def if1(self, b):
+        flag = False
         (l, s, pc) = self.stack.pop(-1)
         self.stack.append((l, s, pc+1))
         if b["condition"] == "gt":
             v1 = s[-1]
             v2 = s[-2]
-            if v1>v2:
+            if v1 > v2:
                 self.log(f"True {v1} > {v2}")
-                return True,b
+                flag = True
             else:
                 self.log(f"False {v1} > {v2}")
         if b["condition"] == "ge":
             v1 = s[-1]
             v2 = s[-2]
-            if v1>=v2:
+            if v1 >= v2:
                 self.log(f"True {v1} >= {v2}")
-                return True,b
+                flag = True
             else:
                 self.log(f"False {v1} >= {v2}")
-            
+
         if b["condition"] == "le":
             v1 = s[-1]
             v2 = s[-2]
-            if v1<=v2:
+            if v1 <= v2:
                 self.log(f"True {v1} <= {v2}")
-                return True,b
+                flag = True
             else:
                 self.log(f"False {v1} <= {v2}")
+
+        if flag:
+            return self.goto(b)
+
         return False, s[-1]
-    
-    def ifz(self,b):
+
+    def ifz(self, b):
         flag = False
         (l, s, pc) = self.stack.pop(-1)
         self.stack.append((l, s, pc+1))
         if b["condition"] == "gt":
             v = s[-1]
-            if v>0:
+            if v > 0:
                 self.log(f"True {v}>0")
                 flag = True
             else:
                 self.log(f"False {v}>0")
-            
+
         if b["condition"] == "le":
             v = s[-1]
-            if v<=0:
+            if v <= 0:
                 self.log(f"True {v}<=0")
                 flag = True
             else:
                 self.log(f"False {v}<=0")
+
         if flag:
             return self.goto(b)
-        
+
         return True, b
-    
-    def goto(self,b):
+
+    def goto(self, b):
         (l, s, pc) = self.stack.pop(-1)
         self.log(f"Going to {b['target']}")
         self.stack.append((l, s, b["target"]))
         return True, b
 
-    def incr(self,b):
+    def incr(self, b):
         (l, s, pc) = self.stack.pop(-1)
         self.log(f"Incrementing {l[b['index']]} by {b['amount']}")
-        l[b["index"]]+=b["amount"]
+        l[b["index"]] += b["amount"]
         self.stack.append((l, s, pc+1))
-        return True,b
-
+        return True, b
 
     def binary(self, b):
         if b["operant"] == "add":
@@ -187,7 +190,7 @@ class Interpreter:
 
             self.log("mul " + str(v1) + " * " + str(v2))
             self.stack.append((l, s[:-2] + [v1*v2], pc + 1))
-        
+
         elif b["operant"] == "div":
             (l, s, pc) = self.stack.pop(-1)
             v1 = s[-1]
@@ -195,14 +198,14 @@ class Interpreter:
 
             self.log("div " + str(v1) + " / " + str(v2))
             self.stack.append((l, s[:-2] + [v1/v2], pc + 1))
-        
+
         elif b["operant"] == "rem":
             (l, s, pc) = self.stack.pop(-1)
             v1 = s[-1]
             v2 = s[-2]
 
             self.log("rem " + str(v1) + " % " + str(v2))
-            self.stack.append((l, s[:-2] + [v1%v2], pc + 1))
+            self.stack.append((l, s[:-2] + [v1 % v2], pc + 1))
         return True, b
 
     def return1(self, b):
@@ -216,17 +219,17 @@ class Interpreter:
         else:
             return False, None
 
-    def get1(self,b): #This should get methods from other classes NOT DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    def get1(self, b):  # This should get methods from other classes NOT DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         (l, s, pc) = self.stack.pop(-1)
         self.log("FUNCTION get NOT IMPLEMENTED")
         self.stack.append((l, s, pc + 1))
-        return True,b
-    
-    def invoke(self,b): #This should use methods from other classes NOT DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        return True, b
+
+    def invoke(self, b):  # This should use methods from other classes NOT DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         (l, s, pc) = self.stack.pop(-1)
         self.log("FUNCTION invoke NOT IMPLEMENTED")
         self.stack.append((l, s, pc + 1))
-        return True,b
+        return True, b
 
     def push(self, b):
         v = b["value"]["value"]
@@ -236,8 +239,37 @@ class Interpreter:
         return True, b
 
 
-for method in methods:
-    intr = Interpreter(method, None)
-    print("Running method: " + method["name"])
-    res = (intr.run(([5, 2, 3, 4][:method["max_locals"]], [], 0)))
-    print("Return: " + str(res))
+def testingSimple():
+    for method in methods:
+        intr = Interpreter(method, None)
+        print("Running method: " + method["name"])
+        if method["name"] == "noop":
+            res = (intr.run(([5, 2, 3, 4][:method["max_locals"]], [], 0)))
+            assert res == None
+            print("Succeded")
+        elif method["name"] == "zero":
+            res = (intr.run(([][:method["max_locals"]], [], 0)))
+            assert res == 0
+            print("Succeded")
+        elif method["name"] == "hundredAndTwo":
+            res = (intr.run(([][:method["max_locals"]], [], 0)))
+            assert res == 102
+            print("Succeded")
+        elif method["name"] == "identity":
+            a = 2
+            res = (intr.run(([a][:method["max_locals"]], [], 0)))
+            assert res == a
+            print("Succeded")
+        elif method["name"] == "add":
+            a = 2
+            b = 4
+            res = (intr.run(([a, b][:method["max_locals"]], [], 0)))
+            assert res == (a+b)
+            print("Succeded")
+        elif method["name"] == "factorial":
+            res = (intr.run(([3, 0][:method["max_locals"]], [], 0)))
+            assert res == 6
+            print("Succeded")
+
+
+testingSimple()
