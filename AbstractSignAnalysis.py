d049483ac1a4d6cfc8dc734b4c833e06f435bcd9
Chetraru Ion <s204710@student.dtu.dk>
2023-10-02 15:28:16 +0200

    AbstractSignAnalysis added

---
+++ AbstractSignAnalysis.py
@@ -0,0 +1,118 @@
+class ArithmeticSignAnalysis:
+    def __init__(self, initial_value):
+        if type(initial_value).__name__ == "int":
+            self.ps = set(self.alpha(initial_value))
+        elif type(initial_value) == "list":
+            self.ps = set([])
+            for val in initial_value:
+                self.ps = set().union(self.ps, self.alpha(val))
+        self.error = set([])
+
+    def alpha(self, var):
+        return self.sign(var)
+
+    def sign(self, var):
+        if var > 0:
+            return {"+"}
+        if var == 0:
+            return {"0"}
+        if var < 0:
+            return {"-"}
+
+    def abstr_add(self, signs):
+        init_signs = self.ps
+        for sign1 in init_signs:
+            for sign2 in signs:
+                if (sign1 == "+" and sign2 == "+") or (sign1 == "+" and sign2 == "0"):
+                    self.ps = set().union(self.ps, {"+"})
+                elif (sign1 == "-" and sign2 == "+") or (sign1 == "+" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"+", "-", "0"})
+                elif (sign1 == "0" and sign2 == "-") or (sign1 == "-" and sign2 == "0") or (sign1 == "-" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"-"})
+                elif (sign1 == "0" and sign2 == "0"):
+                    self.ps = set().union(self.ps, {"0"})
+
+        return self
+#               +   | -        | 0
+#        + |{+}     | {+,-,0}  | {+}
+#        - |{-,0,+} | {-}      | {-}
+#        0 |{+}     | {-}      | {0}
+
+    def abstr_sub(self, signs):
+        init_signs = self.ps
+        for sign1 in init_signs:
+            for sign2 in signs:
+                if (sign1 == "+" and sign2 == "+") or (sign1 == "-" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"+", "-", "0"})
+                elif (sign1 == "-" and sign2 == "+") or (sign1 == "0" and sign2 == "+") or (sign1 == "-" and sign2 == "0"):
+                    self.ps = set().union(self.ps, {"-"})
+                elif (sign1 == "+" and sign2 == "-") or (sign1 == "+" and sign2 == "0") or (sign1 == "0" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"+"})
+                elif (sign1 == "0" and sign2 == "0"):
+                    self.ps = set().union(self.ps, {"0"})
+        return self
+#               +   | -        | 0
+#        + |{+,-,0} | {+}      | {+}
+#        - |{-}     | {-,+,0}  | {-}
+#        0 |{-}     | {+}      | {0}
+
+    def abstr_mul(self, signs):
+        init_signs = self.ps
+        for sign1 in init_signs:
+            for sign2 in signs:
+                if (sign1 == "+" and sign2 == "+") or (sign1 == "-" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"+"})
+                elif sign1 == "0" or sign2 == "0":
+                    self.ps = set().union(self.ps, {"0"})
+                elif (sign1 == "-" and sign2 == "+") or (sign1 == "+" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"-"})
+
+        return self
+
+#               +   | -        | 0
+#        + |{+}     | {-}      | {0}
+#        - |{-}     | {+}      | {0}
+#        0 |{0}     | {0}      | {0}
+
+    def abstr_div(self, signs):
+        init_signs = self.ps
+        for sign1 in init_signs:
+            for sign2 in signs:
+                if (sign1 == "+" and sign2 == "+") or (sign1 == "-" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"+", "0"})
+                elif (sign1 == "-" and sign2 == "+") or (sign1 == "+" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"-", "0"})
+                elif sign2 == "0":
+                    self.error = set().union(self.error, "DivisionZeroException")
+                elif sign1 == "0":
+                    self.ps = set().union(self.ps, {"0"})
+        return self
+
+#               +       | -           | 0
+#        + |{+,0}       | {-,0}       | {ø}
+#        - |{-,0}       | {+,0}       | {ø}
+#        0 |{0}         | {0}         | {ø}
+    def abstr_rem(self, signs):
+        init_signs = self.ps
+        for sign1 in init_signs:
+            for sign2 in signs:
+                if (sign1 == "-" and sign2 == "+") or (sign1 == "-" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"-", "0"})
+                elif (sign1 == "+" and sign2 == "+") or (sign1 == "+" and sign2 == "-"):
+                    self.ps = set().union(self.ps, {"+", "0"})
+                elif sign2 == "0":
+                    self.error = set().union(self.error, "DivisionZeroException")
+                elif sign1 == "0":
+                    self.ps = set().union(self.ps, {"0"})
+        return self
+
+#               +       | -           | 0
+#        + |{+,0}       | {+,0}      | null
+#        - |{-,0}       | {-,0}       | null
+#        0 |{0}         | {0}         | null
+
+    def __str__(self):
+        return str(self.ps)
+
+    def __repr__(self) -> str:
+        return str(self.ps)
